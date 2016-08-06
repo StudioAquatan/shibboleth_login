ea318f0624920d0b94d45113384cf46beec93da7
pddg <s.kokuryo@gmail.com>
2016-08-06 12:09:21 +0900

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+# Login to portal.kit.ac.jp
+
+This module can login to https://portal.student.kit.ac.jp/ and access any other pages limited access to students or 
+teacher of Kyoto Institute of Technology. 
+
+## Requirements
+
+It is as follows. I verified this with Python 3.4.3 in Mac OS X 10.11.16.
+
+* beautifulsoup4==4.5.1
+* lxml==3.6.1
+* requests==2.10.0
+
+
+## Usage
+
+This module require your username and password to auth. Login is very simple as follows.
+
+```python
+from login import ShibbolethClient
+
+client = ShibbolethClient('your username', 'your passsword')
+```
+
+Access other pages
+
+```python
+from login import ShibbolethClient
+
+client = ShibbolethClient('your username', 'your passsword')
+with client:
+    res = client.get('URL which you want to access')
+    print(type(res)) # => <class 'requests.models.Response'>
+```
+Now, you can only use 'get' method in the 'with' statement. Any other method, for example 'post', is not implemented yet. 
+'get' method return 'requests.models.Response' object. So you simply use it as requests module. 
+When you exit 'with' statement, ShibbolethClient class automatically execute 'close()' method. You don't need to execute 'close()' expressly. 
+
---
+++ login.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class ShibbolethClient(object):
+
+    session = requests.session()
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.url = 'https://portal.student.kit.ac.jp/'
+
+    def __enter__(self):
+        self.get()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+    @staticmethod
+    def parse_saml_data(html):
+        soup = BeautifulSoup(html, "lxml")
+        form = soup.find('form')
+        action = form.get('action')
+        input_form = form.div.find_all('input')
+        relay_state = input_form[0].get('value')
+        saml_response = input_form[1].get('value')
+        saml_data = {
+            'RelayState': relay_state,
+            'SAMLResponse': saml_response
+        }
+        return {'action': action, 'saml_data': saml_data}
+
+    def get(self, url=None):
+        # redirect to authentication page
+        if url:
+            login_page = self.session.get(url)
+        else:
+            login_page = self.session.get(self.url)
+
+        if 'auth.cis.kit.ac.jp' not in login_page.url:
+            return login_page
+
+        # post data
+        auth_data = {
+            "j_username": self.username,
+            "j_password": self.password,
+            "_eventId_proceed": ""
+        }
+        auth_res = self.session.post(login_page.url, data=auth_data)
+
+        # parse response
+        res = self.parse_saml_data(auth_res.text)
+
+        # Request Assertion Consumer Service
+        # Redirect to target resource, and respond with target resource.
+        return self.session.post(res['action'], res['saml_data'])
