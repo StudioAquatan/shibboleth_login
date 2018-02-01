a99f0dde5e7a51866ffe4d13f5428a0c7db1b124
pudding <pudding@mail.poyo.info>
2018-02-01 22:12:28 +0900

    separate post data key as class var

--- login.py
+++ login.py
@@ -4,53 +4,53 @@ from bs4 import BeautifulSoup
 
 class ShibbolethClient(object):
 
-    session = requests.session()
+    PARSER = 'lxml'
+    SHIBBOLETH_AUTH_DOMAIN = 'auth.cis.kit.ac.jp'
+    SHIBBOLETH_USERNAME_KEY = 'j_username'
+    SHIBBOLETH_PASSWORD_KEY = 'j_password'
+    SHIBBOLETH_OPTION_DATA = {
+        "_eventId_proceed": ""
+    }
 
-    def __init__(self, username, password, login_url=None, *args, **kwards):
+    def __init__(self, username, password):
+        self.session = requests.session()
         self.username = username
         self.password = password
-        self.url = login_url if login_url else 'https://portal.student.kit.ac.jp/'
 
     def __enter__(self):
-        self.get()
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.session.close()
 
-    @staticmethod
-    def parse_saml_data(html):
-        soup = BeautifulSoup(html, "lxml")
+    def __parse_saml_data(self, html):
+        soup = BeautifulSoup(html, self.PARSER)
         form = soup.find('form')
-        action = form.get('action')
+        action = form.get('action') 
         input_form = form.div.find_all('input')
-        relay_state = input_form[0].get('value')
-        saml_response = input_form[1].get('value')
         saml_data = {
-            'RelayState': relay_state,
-            'SAMLResponse': saml_response
+            'RelayState': input_form[0].get('value'),
+            'SAMLResponse': input_form[1].get('value')
         }
         return {'action': action, 'saml_data': saml_data}
 
-    def get(self, url=None):
+    def get(self, url, *args, **kwards):
         # redirect to authentication page
-        if url:
-            login_page = self.session.get(url, timeout=20)
-        else:
-            login_page = self.session.get(self.url, timeout=20)
+        login_page = self.session.get(url, *args, **kwards)
 
-        if 'auth.cis.kit.ac.jp' not in login_page.url:
+        if self.SHIBBOLETH_AUTH_DOMAIN not in login_page.url:
             return login_page
 
         # post data
         auth_data = {
-            "j_username": self.username,
-            "j_password": self.password,
-            "_eventId_proceed": ""
+            self.SHIBBOLETH_USERNAME_KEY: self.username,
+            self.SHIBBOLETH_PASSWORD_KEY: self.password,
+            **self.SHIBBOLETH_OPTION_DATA
         }
         auth_res = self.session.post(login_page.url, data=auth_data)
 
         # parse response
-        res = self.parse_saml_data(auth_res.text)
+        res = self.__parse_saml_data(auth_res.text)
 
         # Request Assertion Consumer Service
         # Redirect to target resource, and respond with target resource.
